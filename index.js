c13ce7f53613d1fd3f6d7eb96c9523f29ef84605
Miha Rebernik <miha@rebernik.info>
2016-05-21 14:00:34 -0700

    Work on Heroku

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var koa = require('koa');
 var serve = require('koa-static');
 var app = koa();
 app.use(serve('./public'));
-app.listen(3000);
+app.listen(Number.parseInt(process.env.PORT, 10) || 3000);
